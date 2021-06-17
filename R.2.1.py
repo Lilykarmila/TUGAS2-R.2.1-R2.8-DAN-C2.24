13787cd36e789bbbe8b22c1e57bc636bd0f4b859
Lily karmila <58091946+Lilykarmila@users.noreply.github.com>
2021-06-17 13:46:13 +0800

    Update R.2.1.py

--- R.2.1.py
+++ R.2.1.py
@@ -2,4 +2,6 @@
 # contoh perangkat lunak life-critical
 
 # jawab:
-# Robotic Surgery machines(robot untuk operasi di rumah sakit), Railway signaling (sinyal jalur kereta api), flight controllers (pengendali pesawat)
+# 1. Robotic Surgery machines(robot untuk operasi di rumah sakit), 
+# 2. Railway signaling (sinyal jalur kereta api), 
+# 3. flight controllers (pengendali pesawat)
