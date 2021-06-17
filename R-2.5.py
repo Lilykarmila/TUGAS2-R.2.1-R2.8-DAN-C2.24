d8f2e6f90097dbf029149fc6610b383e5081d52e
Lily karmila <58091946+Lilykarmila@users.noreply.github.com>
2021-06-17 14:17:53 +0800

    Update R-2.5.py

--- R-2.5.py
+++ R-2.5.py
@@ -1,3 +1,6 @@
+#Use the techniques of Section 1.7 to revise the charge and make payment methods of 
+#the CreditCard class to ensure that the caller sends a number as a parameter.
+
 class CreditCard:
   """A consumer credit card."""
   
